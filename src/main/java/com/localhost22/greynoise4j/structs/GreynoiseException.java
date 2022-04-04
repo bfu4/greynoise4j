900d7088449719286fa6df7cc60ea48d0600f59c
bfu4 <bella@sleeeepyti.me>
2022-04-04 10:10:44 -0400

    (docs): document the Greynoise exception

--- src/main/java/com/localhost22/greynoise4j/structs/GreynoiseException.java
+++ src/main/java/com/localhost22/greynoise4j/structs/GreynoiseException.java
@@ -4,17 +4,39 @@ import io.vertx.core.buffer.Buffer;
 import io.vertx.ext.web.client.HttpResponse;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * The Greynoise exception is an extension of a runtime exception thrown
+ * when there is an api error.
+ */
 public final class GreynoiseException extends RuntimeException {
 
-    public static GreynoiseException rateLimit(final HttpResponse<Buffer> buffer) {
-        return create("Rated limited!", buffer);
+    /**
+     * Create a rate limit exception with the specified http response.
+     * @param resp the http response
+     * @return greynoise exception to throw
+     */
+    public static GreynoiseException rateLimit(final HttpResponse<Buffer> resp) {
+        return create("Rated limited!", resp);
     }
 
+    /**
+     * Create an exception with the specified message and http response.
+     * @param message  message
+     * @param response response
+     * @return a new Greynoise exception to throw
+     */
     public static GreynoiseException create(final String message, @NotNull final HttpResponse<Buffer> response) {
         return new GreynoiseException(message, response);
     }
 
-    protected GreynoiseException(final String message, @NotNull final HttpResponse<Buffer> response) {
+    /**
+     * Create a new Greynoise exception with the specified message and http response.
+     * This is typically instantiated to throw during a
+     * {@link com.localhost22.greynoise4j.client.Client#request(io.vertx.core.http.HttpMethod, String)}.
+     * @param message  message
+     * @param response response
+     */
+    GreynoiseException(final String message, @NotNull final HttpResponse<Buffer> response) {
         super(message + response.bodyAsString());
     }
 
